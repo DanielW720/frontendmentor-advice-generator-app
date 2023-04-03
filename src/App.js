1a04557f257990884d4cff4871a02d8784cf4069
DW Desktop <westerlund.da@gmail.com>
2023-04-03 22:11:19 +0200

    App complete

--- src/App.js
+++ src/App.js
@@ -10,8 +10,3 @@ function App() {
 }
 
 export default App;
-
-{/* <div class="attribution">
-Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
-Coded by <a href="#">Daniel Westerlund</a>.
-</div> */}
--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,21 +1,39 @@
+import { useEffect, useState } from 'react';
 import styles from './Quote.module.css';
 import Card from './Card';
 import iconDice from './../images/icon-dice.svg';
 import patternDividerMobile from './../images/pattern-divider-mobile.svg';
 import patternDividerDesktop from './../images/pattern-divider-desktop.svg';
 
+const adviceSlipEndpoint = 'https://api.adviceslip.com/advice';
+
 const Quote = () => {
+    const [advice, setAdvice] = useState();
+
+    const handleDiceRoll = async () => {
+        const response = await fetch(adviceSlipEndpoint);
+        const advice = await response.json();
+        setAdvice(advice.slip);
+    }
+
+    // Loads new advice upon first render
+    useEffect(() => {
+        handleDiceRoll();
+    }, []
+    );
+
     return (
         <Card className={styles}>
-            <h2>ADVICE #117</h2>
+            <h2>ADVICE #{advice.id}</h2>
             <p>
-                "It is easy to sit up and take notice,
-                what's difficult is getting up and taking action."
+                {`"${advice.advice}"`}
             </p>
-            <img src={patternDividerMobile} />
-            {/* TODO: Desktop pattern */}
+            <div className={styles.imgContainer}>
+                <img src={patternDividerMobile} alt="Divider" className={styles.imgMobile} />
+                <img src={patternDividerDesktop} alt="Divider" className={styles.imgDesktop} />
+            </div>
             <div className={styles.endOfCard}>
-                <button className={styles.dice}>
+                <button className={styles.dice} onClick={handleDiceRoll}>
                     <img src={iconDice} alt="Dice button" />
                 </button>
             </div>
